bf3a2a99d3fdc1a3bc248fc45c23ffd7e9297262
linclaus <linclaus@outlook.com>
2021-04-08 21:37:30 +0800

    tushare get stock codes

--- build/build.sh
+++ build/build.sh
@@ -3,5 +3,5 @@ set -x
 
 CI_COMMIT_TAG=$(git describe --always --tags)
 
-docker build -t linclaus/stock-exportor:latest -f build/package/Dockerfile .
-docker push linclaus/stock-exportor:latest
+docker build -t linclaus/stock-exportor:$CI_COMMIT_TAG -f build/package/Dockerfile .
+docker push linclaus/stock-exportor:$CI_COMMIT_TAG
