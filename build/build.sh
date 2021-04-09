e95765c67bda2ac1d5d9243301979f3083ff2684
linclaus <linclaus@outlook.com>
2021-04-09 21:53:07 +0800

    build

--- build/build.sh
+++ build/build.sh
@@ -3,5 +3,5 @@ set -x
 
 CI_COMMIT_TAG=$(git describe --always --tags)
 
-docker build -t linclaus/stock-exportor:$CI_COMMIT_TAG -f build/package/Dockerfile .
-docker push linclaus/stock-exportor:$CI_COMMIT_TAG
+docker build -t linclaus/stock-exporter:$CI_COMMIT_TAG -f build/package/Dockerfile .
+docker push linclaus/stock-exporter:$CI_COMMIT_TAG
